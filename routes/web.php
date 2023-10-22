6583d583a4a91dfbc366e5305b4032d0481cc0f7
廖家駒 <3b032107@gm.student.ncut.edu.tw>
2023-10-22 22:43:14 +0800

    新增首頁、關於本站、最新消息的Route

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('home.blade.php');
-//});
-//
-//Route::get('/about', function () {
-//    return view('about.blade.php');
-//});
-//
-//Route::get('/news', function () {
-//    return view('news.blade.php');
-//});
-//
-//Route::get('home/{name?}',['as'=>'home.index',function ($name = 'home'){
-//    return 'Hello, '.$name;
-//}])->name('hello.index');
-//Route::get('about/{name?}',['as'=>'about.index',function ($name = 'about'){
-//    return 'Hello, '.$name;
-//}])->name('about.index');
-//Route::get('news/{name?}',['as'=>'news.index',function ($name = 'news'){
-//    return 'Hello, '.$name;
-//}])->name('news.index');
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\NewsController;
