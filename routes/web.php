7b60c5b018a761851fc208107f31998d87161fe8
廖家駒 <3b032107@gm.student.ncut.edu.tw>
2023-10-22 20:05:40 +0800

    測試

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('{home.blade.php}');
+});
+
+Route::get('/about', function () {
+    return view('{about.blade.php}');
+});
+
+Route::get('/news', function () {
+    return view('{news.blade.php}');
+});
+
+Route::get('hello/{name?}',['as'=>'hello.index',function ($name = 'home'){}]);
+Route::get('about/{name?}',['as'=>'about.index',function ($name = 'about'){}]);
+Route::get('news/{name?}',['as'=>'news.index',function ($name = 'news'){}]);
+
+Route::get('/',['as'=>'home.index','uses'=>'HomeController@index']);
+Route::get('/',['as'=>'about.index','uses'=>'AboutController@index']);
+Route::get('/',['as'=>'news.index','uses'=>'NewsController@index']);
